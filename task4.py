7bc04c4de143dcad754946ce746da9629d023152
Saidakbar Boistov <150612406+Boistov@users.noreply.github.com>
2024-05-23 18:28:21 +0400

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,22 +1,18 @@
 class Stack:
     def __init__(self):
         self.stack = []
+
     def push(self, element):
         self.stack.append(element)
+
     def pop(self):
         if not self.is_empty():
             return self.stack.pop()
         else:
-            return "Error"
+            return "Error: Stack is empty"
+
     def display(self):
         return self.stack
+
     def is_empty(self):
         return len(self.stack) == 0
-stack = Stack()
-stack.push(1)
-stack.push(2)
-stack.push(3)
-print(stack.display())
-popped_element = stack.pop()
-print(popped_element)
-print(stack.display())
